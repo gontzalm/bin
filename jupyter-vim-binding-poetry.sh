6af7019bd44e982d93f897ede365e065411d5e8a
Gontzal Monasterio <go.monasterio@gmail.com>
2021-03-20 00:21:37 +0100

    add shebang

--- jupyter-vim-binding-poetry.sh
+++ jupyter-vim-binding-poetry.sh
@@ -1,4 +1,6 @@
-mkdir -p $CONDA_PREFIX/share/jupyter/nbextensions
-cd $CONDA_PREFIX/share/jupyter/nbextensions
+#!/bin/bash
+
+mkdir $(poetry env info --path)/share/jupyter/nbextensions
+cd $(poetry env info --path)/share/jupyter/nbextensions
 git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
 jupyter nbextension enable vim_binding/vim_binding --sys-prefix
