00a1d8de3949bc054626cb7e70dd997ea81e903a
Gontzal Monasterio <go.monasterio@gmail.com>
2021-05-01 21:16:27 +0200

    allow execution without poetry shell

--- jupyter-vim-binding-poetry.sh
+++ jupyter-vim-binding-poetry.sh
@@ -3,4 +3,4 @@
 mkdir $(poetry env info --path)/share/jupyter/nbextensions
 cd $(poetry env info --path)/share/jupyter/nbextensions
 git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
-jupyter nbextension enable vim_binding/vim_binding --sys-prefix
+poetry run jupyter nbextension enable vim_binding/vim_binding --sys-prefix
