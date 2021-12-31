1c19de25af34ccd83264e28dd2679c58e197b902
gontzalm <go.monasterio@gmail.com>
2021-12-31 14:28:33 +0100

    remove extension

--- set-keymap.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-localectl set-x11-keymap us,es pc104 "" grp:lwin_switch,caps:swapscape
-echo "Successfully set keymap:"
-localectl status
--- test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-poetry run coverage run --source=$(basename $(pwd)) -m pytest
-poetry run coverage report
